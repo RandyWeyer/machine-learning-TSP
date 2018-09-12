0b1d8e3eb3d77ba0f83ee23ea43f39160167b76e
Randy Weyer <randy@>
2018-09-12 10:57:01 -0700

    add brute force functionality

--- js/canvasRender.js
+++ js/canvasRender.js
@@ -1,6 +1,8 @@
 let canvas = new fabric.Canvas('canvas');
 let circle, origX, origY;
 let canvasX = 1000, canvasY = 600;
+var bestWeight = 999999.0;
+var bestPath = [];
 
 let allPoints = [];
 
@@ -15,8 +17,8 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function draw50Points() {
-  for (var i = 0; i < 50; i++) {
+function draw8Points() {
+  for (var i = 0; i < 8; i++) {
         console.log(allPoints);
     origX = getRandomInt(canvasX);
     origY = getRandomInt(canvasY);
@@ -35,8 +37,8 @@ function draw50Points() {
   };
       canvas.renderAll();
 }
-function draw100Points() {
-  for (var i = 0; i < 100; i++) {
+function draw20Points() {
+  for (var i = 0; i < 20; i++) {
         console.log(allPoints);
     origX = getRandomInt(canvasX);
     origY = getRandomInt(canvasY);
@@ -56,8 +58,68 @@ function draw100Points() {
       canvas.renderAll();
 }
 
-function travellingSalesman() {
+const permutator = (inputArr) => {
+  let result = [];
 
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m)
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next))
+     }
+   }
+ }
+
+ permute(inputArr)
+console.log(result);
+ return result;
+}
+
+function currentPermatationWeight(currentPermatation) {
+  var tempDistance = 0.0;
+  // Find the distance of the current Permutation
+  for (var i = 0; i < allPoints.length - 1; i++) {
+    // console.log(allPoints[i][0]);
+    // Distance formula adds to temp distance
+    tempDistance += Math.sqrt(((currentPermatation[i][0] - currentPermatation[i+1][0]) * (currentPermatation[i][0] - currentPermatation[i+1][0])) + ((currentPermatation[i][1] - currentPermatation[i+1][1]) * (currentPermatation[i][1] - currentPermatation[i+1][1])))
+  }
+  // Add distance from the last to the first element
+  // console.log(currentPermatation.length);
+  tempDistance += Math.sqrt(((currentPermatation[currentPermatation.length - 1][0] - currentPermatation[0][0]) * (currentPermatation[currentPermatation.length - 1][0] - currentPermatation[0][0])) + ((currentPermatation[currentPermatation.length - 1][1] - currentPermatation[0][1]) * (currentPermatation[currentPermatation.length - 1][1] - currentPermatation[0][1])))
+  // console.log(tempDistance);
+  return tempDistance;
+}
+
+function bruteForce() {
+  var allPermutations = permutator(allPoints);
+
+  for (var i = 0; i < allPermutations.length; i++) {
+    // console.log(allPermutations[i]);
+    var tempWeight = currentPermatationWeight(allPermutations[i]);
+    if(tempWeight < bestWeight){
+      console.log(tempWeight);
+      bestWeight = tempWeight;
+      bestPath = allPermutations[i];
+    }
+
+  }
+  console.log(bestWeight);
+  console.log(bestPath);
+  bestPath.push(bestPath[0])
+
+  // Draw the Best Path
+  for (var i = 0; i < bestPath.length - 1; i++) {
+    canvas.add(new fabric.Line([bestPath[i][0], bestPath[i][1], bestPath[i+1][0], bestPath[i+1][1]], {
+        stroke: 'red'
+    }));
+    console.log(bestPath[i]);
+  }
+  // canvas.add(new fabric.Line([bestPath[this.length][0], bestPath[this.length][1], bestPath[0][0], bestPath[0][1]], {
+  //     stroke: 'red'
+  // }));
 }
 
 
